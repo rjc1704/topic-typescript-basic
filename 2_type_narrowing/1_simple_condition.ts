14ce11954710ee32f95efe7dec20872aaba0462e
Jecheon Ryu <rjc1704@gmail.com>
2025-05-29 17:22:58 +0900

    타입 심화 파트 시연용

--- 2_type_narrowing/1_simple_condition.ts
+++ 2_type_narrowing/1_simple_condition.ts
@@ -2,16 +2,21 @@
 {
   type User = {
     name: string;
-    age?: number;
+    age?: number | null;
   };
 
   function printUserAge(user: User) {
     if (user.age) {
+      // falsy 값들을 모두 체크
       console.log(`User's age is ${user.age}`);
     } else {
       console.log("User's age is not provided");
     }
   }
-  printUserAge({ name: "John" });
-  printUserAge({ name: "John", age: 30 });
+
+  // 테스트 케이스
+  printUserAge({ name: "abc" }); // age undefined
+  printUserAge({ name: "abc", age: null }); // age null
+  printUserAge({ name: "abc", age: 30 }); // age 30
+  printUserAge({ name: "abc", age: 0 }); // age 0 (주의: falsy값)
 }
