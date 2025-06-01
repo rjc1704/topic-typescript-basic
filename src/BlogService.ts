fb59d61dc7050db7bfd7e98bce8b38b49e7afbaa
Jecheon Ryu <rjc1704@gmail.com>
2025-06-01 13:23:44 +0900

    실습7: 준비중

--- src/BlogService.ts
+++ src/BlogService.ts
@@ -1,46 +1,66 @@
-import { DraftPost, IBlogService, Post } from "./types";
+import { ApiResponse, DraftPost, IBlogService, Post } from "./types";
 
 export class BlogService implements IBlogService {
   private posts: Post[] = [];
   private nextPostId: number = 1;
 
-  // TODO-2: 아래 메서드들의 타입이 실제 API호출처럼 비동기적으로 동작한다고 가정합니다.
-  // 각 메서드들의 리턴타입을 Promise<ApiResponse<T>> 형태로 수정하세요
-  // 메서들의 리턴값은 Promise.resolve() 를 사용하세요.
-
-  getAllPosts(): Post[] {
-    return this.posts;
+  async getAllPosts(): Promise<ApiResponse<Post[]>> {
+    return {
+      success: true,
+      data: this.posts,
+    };
   }
 
-  getPostById(id: number): Post | undefined {
-    return this.posts.find((post) => post.id === id);
+  async getPostById(id: number): Promise<ApiResponse<Post | undefined>> {
+    return {
+      success: true,
+      data: this.posts.find((post) => post.id === id),
+    };
   }
 
-  addPost(postData: Omit<DraftPost, "status">): Post {
+  async addPost(
+    postData: Omit<DraftPost, "status">,
+  ): Promise<ApiResponse<Post>> {
     const post: Post = {
       ...postData,
       status: "draft",
       id: this.nextPostId++,
     };
     this.posts.push(post);
-    return post;
+    return {
+      success: true,
+      data: post,
+    };
   }
 
-  updatePost(
+  async updatePost(
     id: number,
     updateData: Partial<Omit<Post, "id" | "status">>,
-  ): Post | undefined {
-    const post = this.getPostById(id);
-    if (!post) return undefined;
-    const updatedPost = { ...post, ...updateData };
+  ): Promise<ApiResponse<Post | undefined>> {
+    const post = await this.getPostById(id);
+    if (!post.data)
+      return {
+        success: false,
+        data: undefined,
+        error: "Post not found",
+      };
+    const updatedPost = { ...post.data, ...updateData };
     this.posts = this.posts.map((p) => (p.id === id ? updatedPost : p));
-    return updatedPost;
+    return {
+      success: true,
+      data: updatedPost,
+    };
   }
 
-  deletePost(id: number): boolean {
+  async deletePost(id: number): Promise<ApiResponse<boolean>> {
     const index = this.posts.findIndex((post) => post.id === id);
-    if (index === -1) return false;
+    if (index === -1)
+      return {
+        success: false,
+        data: false,
+        error: "Post not found",
+      };
     this.posts.splice(index, 1);
-    return true;
+    return { success: true, data: true };
   }
 }
--- src/types.ts
+++ src/types.ts
@@ -57,23 +57,15 @@ export interface Author extends User {
 }
 
 export interface IBlogService {
-  getAllPosts: () => Post[];
-  getPostById: (id: number) => Post | undefined;
-  addPost: (postData: Omit<DraftPost, "status">) => Post;
+  getAllPosts: () => Promise<ApiResponse<Post[]>>;
+  getPostById: (id: number) => Promise<ApiResponse<Post | undefined>>;
+  addPost: (postData: Omit<DraftPost, "status">) => Promise<ApiResponse<Post>>;
   updatePost: (
     id: number,
     updateData: Partial<Omit<Post, "id" | "status">>,
-  ) => Post | undefined;
-  deletePost: (id: number) => boolean;
+  ) => Promise<ApiResponse<Post | undefined>>;
+  deletePost: (id: number) => Promise<ApiResponse<boolean>>;
 }
-
-// TODO-1:src/types.ts에 제네릭 인터페이스 ApiResponse<T>
-// 속성:
-//   success:
-//   boolean,
-//   data: T,
-//   error?: string
-//   비동기 API 호출 결과를 객체형태의 표준 응답 형태로 사용하기 위해 정의
 
 export interface ApiResponse<T> {
   success: boolean;
