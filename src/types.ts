a4ef46caaa2d91f430db3701048d4389c1085d48
Jecheon Ryu <rjc1704@gmail.com>
2025-06-01 20:17:48 +0900

    실습 완료

--- src/types.ts
+++ src/types.ts
@@ -34,24 +34,10 @@ export type Post = { id: number } & (DraftPost | PublishedPost | ArchivedPost);
 export type GetPostsFunc = (authorId?: number) => Post[];
 export type GetPostByIdFunc = (id: number) => Post | undefined;
 
-// TODO-1: 적절한 유틸리티타입을 사용해서 AddPostFunc의 postData 타입을 수정해보세요
-export type AddPostFunc = (postData: {
-  title: string;
-  content: string;
-  authorId: number;
-  tags: string[];
-}) => Post;
-
-// TODO-2: 적절한 유틸리티타입을 사용해서 UpdatePostFunc의 updateData 타입을 수정해보세요
+export type AddPostFunc = (postData: Omit<DraftPost, "status">) => Post;
 export type UpdatePostFunc = (
   id: number,
-  updateData: {
-    title?: string;
-    content?: string;
-    authorId?: number;
-    tags?: string[];
-    publishedDate?: Date;
-  },
+  updateData: Partial<Omit<Post, "id" | "status">>,
 ) => Post | undefined;
 export type DeletePostFunc = (id: number) => boolean;
 
